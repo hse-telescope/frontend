c2a3a14a835971607e96bef06847d4e7153a28bf
Viktor Krivoshchekov <robivirt@mail.ru>
2025-02-03 20:43:11 +0300

    add front2back

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+// import React, {useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactFlow, {
   addEdge,
   applyNodeChanges,
@@ -12,28 +13,73 @@ import ReactFlow, {
   MarkerType,
   NodeChange,
   EdgeChange,
+  NodeDragHandler
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 import Toolbar from './components/Toolbar/Toolbar';
 import NodeModal from './components/NodeModal/NodeModal';
 import EdgeModal from './components/EdgeModal/EdgeModal';
 import './index.css';
-//import axios from 'axios';
+import axios from 'axios';
 
 const App: React.FC = () => {
 
-  useEffect(() => {
-    axios.get("/api/v1/ping").then((message) => {
-      // alert(JSON.stringify(message))
-      alert(message.status + "" + JSON.stringify(message.data));
-    });
-  }, []);
+  // useEffect(() => {
+  //   axios.get("/api/v1/ping").then((message) => {
+  //     // alert(JSON.stringify(message))
+  //     alert(message.status + "" + JSON.stringify(message.data));
+  //   });
+  // }, []);
 
   const [nodes, setNodes] = useState<Node[]>([]);
   const [edges, setEdges] = useState<Edge[]>([]);
 
   const [nodeIdCounter, setNodeIdCounter] = useState(1);
   const [edgeIdCounter, setEdgeIdCounter] = useState(1);
+
+  useEffect(() => {
+    axios.get("/api/v1/graph/1").then((message) => {
+      // alert(JSON.stringify(message))
+      alert(message.status + "" + JSON.stringify(message.data));
+      var servs = JSON.parse(JSON.stringify(message.data))["services"];
+      var rels = JSON.parse(JSON.stringify(message.data))["relations"];
+      console.log(servs)
+
+      console.log(rels)
+      var newservs : Node[] = [], newrels: Edge[] = [];
+      var new_serv_id = parseInt(JSON.parse(JSON.stringify(message.data))["max_node_id"])
+      var new_rel_id = parseInt(JSON.parse(JSON.stringify(message.data))["max_edge_id"])
+
+      for (var i = 0; i < servs.length; i++) {
+        var node = {
+          "id" : servs[i]["id"].toString(),
+          "position" : {"x": servs[i]["x"], "y": servs[i]["y"]},
+          "data" : {"label": servs[i]["name"], "description": servs[i]["description"]}
+        }
+
+        newservs.push(node)
+        
+      }
+
+      for (var i = 0; i < rels.length; i++) {
+        var edge = {
+          "id" : rels[i]["id"].toString(),
+          "data" : {"label": rels[i]["name"], "description": rels[i]["description"]},
+          "source" : rels[i]["from_service"].toString(),
+          "target" : rels[i]["to_service"].toString()
+        }
+
+        newrels.push(edge)
+      }
+      console.log(newservs)
+      console.log(newrels)
+
+      setNodeIdCounter(new_serv_id)
+      setEdgeIdCounter(new_rel_id)
+      setNodes(newservs);
+      setEdges(newrels);
+    });
+  }, []);
 
   const [activeNode, setActiveNode] = useState<Node | null>(null);
   const [activeEdge, setActiveEdge] = useState<Edge | null>(null);
@@ -64,6 +110,15 @@ const App: React.FC = () => {
         data: { label: `Node ${nodeIdCounter}` },
       },
     ]);
+    axios.post("/api/v1/services", {
+      "id" : nodeIdCounter,
+      "graph_id" : 1,
+      "name" : `Node ${nodeIdCounter}`,
+      "description" : "",
+      "x" : x,
+      "y" : y
+    });
+    
     console.log("Nodes: ", nodes, "\nEdges: ", edges);
     setNodeIdCounter((prev) => prev + 1);
   };
@@ -88,6 +143,15 @@ const App: React.FC = () => {
         prevEdges
       )
     );
+    axios.post("/api/v1/relations", {
+      "id" : edgeIdCounter,
+      "graph_id" : 1,
+      "name" : `${edgeIdCounter}`,
+      "description" : "",
+      "from_service" : connection.source != null ? parseInt(connection.source) : 1,
+      "to_service" : connection.target != null ? parseInt(connection.target) : 1,
+    });
+    console.log("Nodes: ", nodes, "\nEdges: ", edges);
     setEdgeIdCounter((prev) => prev + 1);
   };
 
@@ -128,6 +192,14 @@ const App: React.FC = () => {
             : node
         )
       );
+      axios.put(`/api/v1/services/${activeNode.id}`, {
+        "id" : parseInt(activeNode.id),
+        "name" : nodeData.name,
+        "description" : nodeData.description,
+        "graph_id": 1,
+        "x" : activeNode.position.x,
+        "y" : activeNode.position.y
+      })
       console.log("Nodes: ", nodes, "\nEdges: ", edges);
       closeModal();
     }
@@ -147,6 +219,15 @@ const App: React.FC = () => {
             : edge
         )
       );
+
+      axios.put(`/api/v1/relations/${activeEdge.id}`, {
+        "id" : parseInt(activeEdge.id),
+        "name" : edgeData.name,
+        "description" : edgeData.description,
+        "graph_id": 1,
+        "from_service" : parseInt(edgeData.from),
+        "to_service" : parseInt(edgeData.to)
+      })
       console.log("Nodes: ", nodes, "\nEdges: ", edges);
       closeModal();
     } else {
@@ -177,6 +258,7 @@ const App: React.FC = () => {
     if (activeNode) {
       setNodes((prevNodes) => prevNodes.filter((node) => node.id !== activeNode.id));
       setEdges((prevEdges) => prevEdges.filter((edge) => edge.source !== activeNode.id && edge.target !== activeNode.id));
+      axios.delete(`/api/v1/services/${activeNode.id}`)
       console.log("Nodes: ", nodes, "\nEdges: ", edges);
       closeModal();
     }
@@ -185,9 +267,23 @@ const App: React.FC = () => {
   const deleteEdge = () => {
     if (activeEdge) {
       setEdges((prevEdges) => prevEdges.filter((edge) => edge.id !== activeEdge.id));
+      axios.delete(`/api/v1/relations/${activeEdge.id}`)
       console.log("Nodes: ", nodes, "\nEdges: ", edges);
       closeModal();
     }
+  };
+
+  const onNodeDragStop: NodeDragHandler = (_, node) => {
+    console.log("Node stopped:", node.id, "Position:", node.position);
+  
+    axios.put(`/api/v1/services/${node.id}`, {
+      "id" : parseInt(node.id),
+      "name" : node.data.label,
+      "description" : node.data.description,
+      "graph_id": 1,
+      "x" : node.position.x,
+      "y" : node.position.y
+    })
   };
   
 
@@ -209,6 +305,7 @@ const App: React.FC = () => {
             onNodeClick={onNodeClick}
             onEdgeClick={onEdgeClick}
             onPaneClick={closeModal}
+            onNodeDragStop={onNodeDragStop}
             fitView
           >
             <defs>
