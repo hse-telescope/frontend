8fd13ac9143ace7d3312ae1a19231b394d95ede6
vladlim <vladlim2003@gmail.com>
2025-05-29 10:04:05 +0300

    Redirect to auth, logout added

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,40 +1,14 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
 
-import React, { useEffect, useState } from "react";
-// import { BrowserRouter as Navigate } from 'react-router-dom';
+const ProtectedRoute: React.FC = () => {
+  const accessToken = localStorage.getItem('accessToken');
+  
+  if (!accessToken) {
+    return <Navigate to="/auth" replace />;
+  }
 
-const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
-    // const navigate = useNavigate();
-  
-    useEffect(() => {
-      const checkAuth = async () => {
-        const accessToken = localStorage.getItem('accessToken');
-        if (!accessToken) {
-          setIsAuthenticated(false);
-          return;
-        }
-  
-        try {
-          setIsAuthenticated(true);
-        } catch (error) {
-          localStorage.removeItem('accessToken');
-          localStorage.removeItem('refreshToken');
-          setIsAuthenticated(false);
-        }
-      };
-  
-      checkAuth();
-    }, []);
-  
-    if (isAuthenticated === null) {
-      return <div>Loading...</div>;
-    }
-  
-    // if (!isAuthenticated) {
-    //   return <Navigate to="/auth" replace />;
-    // }
-  
-    return <>{children}</>;
-  };
-  
-  export default ProtectedRoute;
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
